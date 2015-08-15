0faf341789fd74e8faf8f1e513ce42c5dddd0195
ddc <diaz_carrete@yahoo.com>
2015-08-15 19:24:24 +0200

    generalize, simplify, foldify

--- src/Control/Foldl/Transduce.hs
+++ src/Control/Foldl/Transduce.hs
@@ -3,24 +3,34 @@
 module Control.Foldl.Transduce (
         Transducer 
     ,   TransducerM
+    ,   TransducerG
     ,   Wrap(..)
     ,   WrapM(..)
     ,   transducer
     ,   transducer'
     ,   transducerM
     ,   transducerM'
+    ,   generalizeWrap
+    ,   simplifyWrap
+    ,   foldify
+    ,   foldifyM
     ,   module Control.Foldl
     ) where
 
 import Data.Bifunctor
+import Data.Functor.Identity
 import Data.Foldable (foldrM)
 import Control.Monad
 import Control.Foldl (Fold(..),FoldM(..))
 import qualified Control.Foldl as L
 
+data Pair a b = Pair !a !b
+
 type Transducer a b = forall x. L.Fold b x -> L.Fold a x
 
 type TransducerM m a b = forall x. Monad m => L.FoldM m b x -> L.FoldM m a x
+
+type TransducerG a b = forall x m. Monad m => L.FoldM m b x -> L.FoldM m a x
 
 data Wrap i o r
      = forall x. Wrap (x -> i -> (x,[o])) x (x -> (r,[o]))
@@ -75,6 +85,26 @@ transducerM' f (WrapM wstep wstate wdone) (FoldM fstep fstate fdone) =
                 (wr,os) <- wdone ws
                 liftM (f wr) (fdone =<< foldrM (flip fstep) fs os)
 
-data Pair a b = Pair !a !b
 
+generalizeWrap :: Monad m => Wrap i o r -> WrapM m i o r
+generalizeWrap (Wrap step begin done) = WrapM step' begin' done'
+  where
+    step' x a = return (step x a)
+    begin'    = return  begin
+    done' x   = return (done x)
+
+simplifyWrap :: WrapM Identity i o r -> Wrap i o r
+simplifyWrap (WrapM step begin done) = Wrap step' begin' done'
+  where
+    step' x a = runIdentity (step x a)
+    begin'    = runIdentity  begin
+    done' x   = runIdentity (done x)
+
+foldify :: Wrap i o r -> Fold i r
+foldify (Wrap step begin done) =
+    Fold (\x i -> fst (step x i)) begin (\x -> fst (done x))
+
+foldifyM :: Functor m => WrapM m i o r -> FoldM m i r
+foldifyM (WrapM step begin done) =
+    FoldM (\x i -> fmap fst (step x i)) begin (\x -> fmap fst (done x))
 
