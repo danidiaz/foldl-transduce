750be52170ceb0a2e9a4845a746288831819e46c
ddc <diaz_carrete@yahoo.com>
2015-09-13 20:28:25 +0200

    updated example

--- examples/CharsWordsLinesSentences.hs
+++ examples/CharsWordsLinesSentences.hs
@@ -2,7 +2,8 @@
 
 -- |
 --
--- Count characters, words an lines of the file passed as commad line argument.
+-- Count characters, words, lines and sentences in the file passed as commad
+-- line argument.
 module Main (
         main
     ) where
@@ -13,18 +14,19 @@ import qualified "text" Data.Text as T
 
 import qualified "foldl" Control.Foldl as L
 import "foldl-transduce" Control.Foldl.Transduce
-import "foldl-transduce" Control.Foldl.Transduce.Text (utf8lenient,words,lines)
+import "foldl-transduce" Control.Foldl.Transduce.Text (utf8lenient,words,lines,textualSplit)
 import "foldl-transduce" Control.Foldl.Transduce.ByteString.IO (driveHandle)
 
 import System.IO
 import System.Environment (getArgs)
 
-multicount :: Fold T.Text (Int,Int,Int)
-multicount = (,,) <$> countchars <*> countwords <*> countlines
+multicount :: Fold T.Text (Int,Int,Int,Int)
+multicount = (,,,) <$> countchars <*> countwords <*> countlines <*> countsentences
   where
     countchars = L.premap T.length L.sum
     countwords = folds words unit L.length
     countlines = folds lines unit L.length
+    countsentences = folds (textualSplit (=='.')) unit L.length
 
 -- wget https://www.gutenberg.org/files/2650/2650-0.txt
 main :: IO ()
