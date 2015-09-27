b60f271a075e26092bd325b127212708aa4b18c5
ddc <diaz_carrete@yahoo.com>
2015-09-27 13:31:39 +0200

    criterion benchmark

--- benchmarks/benchmarks.hs
+++ benchmarks/benchmarks.hs
@@ -1,8 +1,21 @@
 module Main where
 
+import Data.Foldable
 import qualified Control.Foldl as L
-import qualified Control.Foldl.Transduce
+import Control.Foldl.Transduce
+import Lens.Family
 
--- TBD
+import Criterion.Main
+
 main :: IO ()
-main = return ()
+main = defaultMain [
+        bgroup "sum" [
+             bench "without trans" (nf 
+                (L.fold L.sum)
+                (take 500000 (cycle [1::Int,-1])))
+        ,
+             bench "with trans"  (nf 
+                (L.fold (folds (chunksOf 1) L.list (L.handles (folding toList) L.sum)))
+                (take 500000 (cycle [1::Int,-1])))
+        ]        
+    ]
